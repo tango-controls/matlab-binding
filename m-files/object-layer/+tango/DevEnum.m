3aa022e231bd032b86b48ba8a3dac68b95dd40f1
nleclerc <nicolas.leclercq@esrf.fr>
2021-02-09 19:44:18 +0100

    added support for DevEnum labels containing special characters

--- m-files/object-layer/+tango/DevEnum.m
+++ m-files/object-layer/+tango/DevEnum.m
@@ -1,39 +1,40 @@
+
 classdef DevEnum
   
-  % Device Interface to Tango devices
-  %
-  % Constructor:
-  %   >> de = tango.DevEnum(device_name, attr_name);
-  
-  properties
-    device;
-    attribute;
-    labels;
-    values;
-    value2label;
-    label2value;
-  end
-  
-  methods
-    % DevEnum constructor
-    % ------------------------------------------------
-    function o = DevEnum(device_name, attribute_name)
-      ac = tango_get_attribute_config(device_name, attribute_name);
-      assert(tango_error == 0 && ac.data_type ==  29);
-      num_labels = size(ac.enum_labels.labels, 2);
-      o.device = device_name;
-      o.attribute = attribute_name;
-      o.labels = struct();
-      o.values = int16(zeros(1, num_labels));
-      o.value2label = containers.Map("KeyType", "uint32", "ValueType", "char");
-      o.label2value = containers.Map("KeyType", "char", "ValueType", "uint32");
-      for l = 1:num_labels;
-        o.labels.(ac.enum_labels.labels{l}) = int16(ac.enum_labels.ids(l));
-        o.values(l) = int16(ac.enum_labels.ids(l));
-        o.value2label(uint32(ac.enum_labels.ids(l))) = ac.enum_labels.labels{l};
-        o.label2value(ac.enum_labels.labels{l}) = uint32(ac.enum_labels.ids(l));
-      end
+    % Device Interface to Tango devices
+    %
+    % Constructor:
+    %   >> de = tango.DevEnum(device_name, attr_name);
+
+    properties
+        device;
+        attribute;
+        labels;
+        values;
+        value2label;
+        label2value;
     end
-  end % methods
+    
+    methods
+        % DevEnum constructor
+        % ------------------------------------------------
+        function obj = DevEnum(device_name, attribute_name)
+            ac = tango_get_attribute_config(device_name, attribute_name);
+            assert(tango_error == 0 && ac.data_type ==  29);
+            num_labels = size(ac.enum_labels.labels, 2);
+            obj.device = device_name;
+            obj.attribute = attribute_name;
+            obj.labels = tango.DevEnumLabels(ac.enum_labels);
+            obj.values = int16(zeros(1, num_labels));
+            obj.value2label = containers.Map("KeyType", "uint32", "ValueType", "char");
+            obj.label2value = containers.Map("KeyType", "char", "ValueType", "uint32");
+            for l = 1:num_labels
+                obj.values(l) = int16(ac.enum_labels.ids(l));
+                obj.value2label(uint32(ac.enum_labels.ids(l))) = ac.enum_labels.labels{l};
+                obj.label2value(ac.enum_labels.labels{l}) = uint32(ac.enum_labels.ids(l));
+            end
+        end
+    end % methods
+   
 
 end % classdef
